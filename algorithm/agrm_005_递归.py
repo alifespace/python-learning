2cc96d1466734cc3b820ff317f5bef871f79e8e1
lifespace <lifespace@qq.com>
2024-04-24 14:08:43 -0700

    new file:   "algorithm/agrm_005_\351\200\222\345\275\222.py" 	new file:   "algorithm/agrm_006_\346\261\211\350\257\272\345\241\224.py"

---
+++ algorithm/agrm_005_递归.py
@@ -0,0 +1,6 @@
+def func(x):
+    if(x>0):
+        print(x)
+        func(x-1)
+
+func(10)
---
+++ algorithm/agrm_006_汉诺塔.py
@@ -0,0 +1,12 @@
+gTotal = 0
+
+def Hanoi(n, a, b, c):
+    global gTotal
+    if(n > 0):
+        Hanoi(n-1, a, c, b)
+        print("moving from %s to %s" % (a, c))
+        gTotal = gTotal + 1
+        Hanoi(n-1, b, a, c)
+
+Hanoi(22, 'A', 'B', 'C')
+print("Total Steps: %s" %gTotal)
