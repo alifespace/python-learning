02269d23766497ac26126f7f1c86efe8b6b23133
lifespace <lifespace@qq.com>
2024-11-19 14:59:45 +0000

    new file:   algorithm/test.ipynb 	new file:   jupyter.log 	modified:   test.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-from openai import OpenAI
-
-client = OpenAI(api_key="sk-94c3efc401af4b87be01a4ecce532bcc", base_url="https://api.deepseek.com")
-
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "如何做炸酱面"},
-    ],
-    stream=False
-)
-
-print(response.choices[0].message.content)
+print("Hello Podman")
