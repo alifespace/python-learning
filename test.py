9267838a279be558634f342e5baeab7619864248
lifespace <lifespace@qq.com>
2024-11-25 22:51:28 +0000

    deleted:    .vscode/settings.json 	modified:   test.ipynb 	modified:   test.py 	modified:   test1.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello Podman")
+from datetime import datetime
+import pytz
+
+utc_now = datetime.now(tz=pytz.utc)
+print("UTC now: ", utc_now)
+
+tc_now.astimezone(pytz.timezone('US/Pacific'))
+print("Pacific time now: ", pacific_time.strftime("%Y-%m-%d %H:%M:%S %z"))
+
+d
