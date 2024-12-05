540c7340ef09eb7cb99e1cf9eb854d03aeedaa69
lifespace <lifespace@qq.com>
2024-12-05 05:09:56 +0000

    modified:   test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import pytz
 utc_now = datetime.now(tz=pytz.utc)
 print("UTC now: ", utc_now)
 
-tc_now.astimezone(pytz.timezone('US/Pacific'))
+pacific_time = utc_now.astimezone(pytz.timezone('US/Pacific'))
 print("Pacific time now: ", pacific_time.strftime("%Y-%m-%d %H:%M:%S %z"))
 
-d
